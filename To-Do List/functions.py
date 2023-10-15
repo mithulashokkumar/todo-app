324c460efbda9a26b957f2597d2f0260609fc0ff
mithul <mithul.ashokkumar@gmail.com>
2023-10-15 11:44:52 -0600

    Initial commit

---
+++ To-Do List/functions.py
@@ -0,0 +1,14 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Reads a text file and returns the list of the to-do items. """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Writes the to-do items list in the text file. """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
