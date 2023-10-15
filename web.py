52b024a9dbca550af089655281e282f87f533233
mithul <mithul.ashokkumar@gmail.com>
2023-10-15 14:51:52 -0600

    Finishing the app

--- web.py
+++ web.py
@@ -14,8 +14,14 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is made to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Enter a task:", placeholder="Write something here...",
               on_change=add_todo, key="new_todo")
